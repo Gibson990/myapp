6207f4973e8bca48786794f94b643f74f4e6235b
gibson.raphael990 <gibson.raphael990@gmail.com>
2024-05-23 08:05:26 +0000

    slide_view fixed

--- lib/slide_view.dart
+++ lib/slide_view.dart
@@ -9,60 +9,90 @@ class SlideView extends StatefulWidget {
 
 class _SlideViewState extends State<SlideView> {
   int _currentIndex = 0;
+  PageController _pageController = PageController();
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  void _onIndicatorTap(int index) {
+    _pageController.animateToPage(
+      index,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Container(
           margin: EdgeInsets.symmetric(vertical: 12.0),
           width: double.infinity,
-          height: 180.0,
-          color: Color(0xFFFF7F00),
-          child: ListView(
-            scrollDirection: Axis.horizontal,
+          height: 220.0, // Adjusted height to accommodate indicators
+          child: Stack(
+            alignment: Alignment.center,
             children: [
-              SlideItem(
-                title: 'Air Cargo Shipping',
-                body:
-                    'Get your goods to their destination quickly and efficiently with our air cargo shipping service.',
-                buttonText: 'Get Started',
-                image: 'assets/slideview/air_cargo_image.png',
+              Positioned.fill(
+                child: PageView(
+                  controller: _pageController,
+                  onPageChanged: _onPageChanged,
+                  children: [
+                    SlideItem(
+                      title: 'Air Cargo Shipping',
+                      body: 'Get your goods to their destination quickly and efficiently with our air cargo shipping service.',
+                      buttonText: 'Get Started',
+                      image: 'assets/slideview/air_cargo_image.png',
+                    ),
+                    SlideItem(
+                      title: 'Sea Freight Shipping',
+                      body: 'Move your goods by sea and save money with our reliable and cost-effective sea freight shipping service.',
+                      buttonText: 'Learn More',
+                      image: 'assets/slideview/sea_freight_image.png',
+                    ),
+                    SlideItem(
+                      title: 'Local Delivery Services',
+                      body: 'Need to get your goods delivered locally? Look no further than our reliable and affordable local delivery service.',
+                      buttonText:  'Book Now',
+                      image: 'assets/slideview/local_delivery_image.png',
+                    ),
+                  ],
+                ),
               ),
-              SlideItem(
-                title: 'Sea Freight Shipping',
-                body:
-                    'Move your goods by sea and save money with our reliable and cost-effective sea freight shipping service.',
-                buttonText: 'Learn More',
-                image: 'assets/slideview/sea_freight_image.png',
-              ),
-              SlideItem(
-                title: 'Local Delivery Services',
-                body:
-                    'Need to get your goods delivered locally? Look no further than our reliable and affordable local delivery service.',
-                buttonText: 'Book Now',
-                image: 'assets/slideview/local_delivery_image.png',
+              Positioned(
+                bottom: 8.0,
+                top: 116.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(3, (index) {
+                    return GestureDetector(
+                      onTap: () {
+                        _onIndicatorTap(index);
+                      },
+                      child: Container(
+                        width: 10.0,
+                        height: 10.0,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _currentIndex == index
+                              ? Colors.white
+                              : Colors.white.withOpacity(0.5),
+                          border: _currentIndex == index
+                              ? Border.all(color: Colors.white, width: 2.0)
+                              : null,
+                        ),
+                      ),
+                    );
+                  }),
+                ),
               ),
             ],
           ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: List.generate(3, (index) {
-            return Container(
-              width: 10.0,
-              height: 10.0,
-              margin: EdgeInsets.symmetric(horizontal: 2.0),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: _currentIndex == index ? Colors.white : Colors.white.withOpacity(0.5),
-                border: _currentIndex == index
-                    ? Border.all(color: Colors.white, width: 2.0)
-                    : null,
-              ),
-            );
-          }),
         ),
       ],
     );
@@ -85,75 +115,77 @@ class SlideItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 320.0,
-      margin: EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12.0),
-      ),
+    return Center(
       child: Stack(
         children: [
-          Image.asset(
-            image,
-            width: 320.0,
-            height: 180.0,
-            fit: BoxFit.cover,
+          Container(
+            width: 380.0, // Increased card width
+            margin: EdgeInsets.symmetric(horizontal: 8.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4.0), // Rounded corners set to 4px
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(4.0), // Apply rounded corners to the image
+              child: Image.asset(
+                image,
+                width: 380.0,
+                height: 180.0,
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
           Container(
-            width: 320.0,
+            width: 380.0,
             height: 180.0,
             decoration: BoxDecoration(
               color: Colors.black.withOpacity(0.18), // Overlay with 18% opacity
-              borderRadius: BorderRadius.circular(12.0),
+              borderRadius: BorderRadius.circular(4.0),
             ),
           ),
           Padding(
             padding: EdgeInsets.all(16.0),
-            child: SizedBox(
-              height: 100, // Set a fixed height or use Expanded to fit the content
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white, // Text color
-                    ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white, // Text color
                   ),
-                  SizedBox(height: 8),
-                  Text(
-                    body,
-                    style: TextStyle(
-                      fontSize: 14, // Increased font size to 14
-                      color: Colors.white, // Text color
-                    ),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  body,
+                  style: TextStyle(
+                    fontSize: 14, // Increased font size to 14
+                    color: Colors.white, // Text color
                   ),
-                  SizedBox(height: 16),
-                  SizedBox(
-                    width: 120.0, // Button width
-                    height: 36.0, // Button height
-                    child: TextButton(
-                      onPressed: () {},
-                      style: TextButton.styleFrom(
-                        padding: EdgeInsets.symmetric(vertical: 8.0),
-                        backgroundColor: Color(0xFFFF7F00),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.zero,
-                        ),
+                ),
+                SizedBox(height: 16),
+                SizedBox(
+                  width: 120.0, // Button width
+                  height: 36.0, // Button height
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      backgroundColor: Color(0xFFFF7F00),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
                       ),
-                      child: Text(
-                        buttonText,
-                        style: TextStyle(
-                          color: Colors.white, // Text color
-                          fontSize: 14, // Text size
-                        ),
+                    ),
+                    child: Text(
+                      buttonText,
+                      style: TextStyle(
+                        color: Colors.white, // Text color
+                        fontSize: 14, // Text size
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ],
