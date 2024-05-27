08ff77e7c8f6b32024f9fa5b9a9581dd19f92eee
gibson.raphael990 <gibson.raphael990@gmail.com>
2024-05-27 12:02:57 +0000

    barcode fix try

---
+++ lib/quick_action_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class QuickActionButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const QuickActionButton({
+    required this.icon,
+    required this.text,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 84,
+      height: 80,
+      padding: const EdgeInsets.all(2),
+      margin: const EdgeInsets.all(2),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            size: 32,
+            color: Theme.of(context).primaryColor,
+          ),
+          const SizedBox(height: 2),
+          Text(
+            text,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
